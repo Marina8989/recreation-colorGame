01cfdcd3a1e5047c1dfc2776edd20ed33fc6d7e9
Marina8989 <marina.mill209@gmail.com>
2020-04-11 14:45:45 -0700

    working

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-let colors = [
-    'rgb(255, 0, 0)',
-    'rgb(255, 255, 0)',
-    'rgb(0, 255, 0)',
-    'rgb(0, 255, 255)',
-    'rgb(0, 0, 255)',
-    'rgb(255, 0, 255)'
-];
+let colors = getRandomColor(6);
 
 let squares = document.querySelectorAll('.square');
 let colorDisplay = document.querySelector('.colorDisplay');
 let messageDisplay = document.querySelector('#message');
 
-let pickedColor = colors[3];
+let pickedColor = pickColor();
 colorDisplay.textContent = pickedColor;
 
 for (i = 0; i < squares.length; i++) {
@@ -34,4 +27,25 @@ function changedColor(color) {
     for (i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = color;
     }
+}
+
+function pickColor() {
+    let random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function getRandomColor(num) {
+    let arr = [];
+    for (i = 0; i < num; i++) {
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var m = "rgb(" + r + ', ' + g + ', ' + b + ")";
+    return m;
 }
