d127358e25f6826f21153804798ab2221f55b8b3
Marina8989 <marina.mill209@gmail.com>
2020-04-11 15:17:18 -0700

    working

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let colors = getRandomColor(6);
 let squares = document.querySelectorAll('.square');
 let colorDisplay = document.querySelector('.colorDisplay');
 let messageDisplay = document.querySelector('#message');
+let h1 = document.querySelector('h1');
 
 let pickedColor = pickColor();
 colorDisplay.textContent = pickedColor;
@@ -16,6 +17,7 @@ for (i = 0; i < squares.length; i++) {
         if (changeColor === pickedColor) {
             messageDisplay.textContent = 'Correct!!!';
             changedColor(pickedColor);
+            h1.style.backgroundColor = pickedColor;
         } else {
             messageDisplay.textContent = 'Try Again!';
             this.style.backgroundColor = '#232323';
